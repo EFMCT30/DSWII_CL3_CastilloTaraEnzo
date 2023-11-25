a9237296fd5b3ddab75a472a2643c854ba3c2f22
efmct30 <enzofranchesco.ec@gmail.com>
2023-11-25 18:10:28 -0500

    primer

---
+++ src/main/java/com/example/dswii_cl3_castillotaraenzo/DswiiCl3CastilloTaraEnzoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dswii_cl3_castillotaraenzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3CastilloTaraEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3CastilloTaraEnzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dswii_cl3_castillotaraenzo/DswiiCl3CastilloTaraEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dswii_cl3_castillotaraenzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3CastilloTaraEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
